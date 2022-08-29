a20ba20856be49612abadc456f67a3a87e4e5b35
jnnanni <jnnanni@umich.edu>
2022-08-29 12:13:47 -0400

    Changed drive controls to better fit start of project

--- src/components/DriveControls.vue
+++ src/components/DriveControls.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <p>Drive Controls Present</p>
-    </div>
+  <div>
+      <p>Drive Controls</p>
+  </div>
 </template>
 
 <script>
@@ -10,60 +10,59 @@
 let interval;
 
 export default {
-  data () {
-    return {
-      dampen: 0,
-      reverse: false
-    }
-  },
+data () {
+  return {
+    rotational: 0,
+    linear: 0
+  }
+},
 
 
-  beforeDestroy: function () {
-    window.clearInterval(interval);
-  },
+beforeDestroy: function () {
+  window.clearInterval(interval);
+},
 
-  created: function () {
+created: function () {
 
-    const JOYSTICK_CONFIG = {
-      'left_right': 0,
-      'forward_back': 1,
-      'twist': 2,
-      'dampen': 3,
-      'pan': 4,
-      'tilt': 5
-    }
+  const JOYSTICK_CONFIG = {
+    'left_right': 0,
+    'forward_back': 1,
+    'twist': 2,
+    'dampen': 3,
+    'pan': 4,
+    'tilt': 5
+  }
 
 
-    const updateRate = 0.05;
-    interval = window.setInterval(() => {
-        const gamepads = navigator.getGamepads()
-        for (let i = 0; i < 4; i++) {
-          const gamepad = gamepads[i]
-          if (gamepad) {
-            if (gamepad.id.includes('Logitech')) {
-              // Make dampen 1 when slider is pushed forward, 0 when pulled back
-              // Raw value is -1 to 1
-              this.dampen = gamepad.axes[JOYSTICK_CONFIG['dampen']] * -0.5 + 0.5
-              // -1 multiplier to make turning left a positive value
-              let rotation = -1 * (gamepad.axes[JOYSTICK_CONFIG['left_right']] + gamepad.axes[JOYSTICK_CONFIG['twist']])
-              if (rotation > 1) {
-                rotation = 1
-              }
-              else if (rotation < -1) {
-                rotation = -1
-              }
-              const joystickData = {
-                'type': 'Joystick',
-                // forward on joystick is -1, so invert
-                'forward_back': -1 * gamepad.axes[JOYSTICK_CONFIG['forward_back']],
-                'left_right': rotation,
-                'dampen': this.dampen
-              }
+  const updateRate = 0.05;
+  interval = window.setInterval(() => {
+      const gamepads = navigator.getGamepads()
+      for (let i = 0; i < 4; i++) {
+        const gamepad = gamepads[i]
+        if (gamepad) {
+          if (gamepad.id.includes('Logitech')) {
+            // -1 multiplier to make turning left a positive value
+            // Both left_right axis and twisting the joystick will turn
+            this.rotational = -1 * (gamepad.axes[JOYSTICK_CONFIG['left_right']] + gamepad.axes[JOYSTICK_CONFIG['twist']])
+            // Range Constraints
+            if (this.rotational > 1) {
+              this.rotational = 1
             }
+            else if (this.rotational < -1) {
+              this.rotational = -1
+            }
+            // forward on joystick is -1, so invert
+            this.linear = -1 * gamepad.axes[JOYSTICK_CONFIG['forward_back']]
+            const joystickData = {
+              'forward_back': this.linear,
+              'left_right': this.rotational,
+            }
+            
           }
         }
-    }, updateRate*1000)
-  },
+      }
+  }, updateRate*1000)
+},
 
 }
 </script>
