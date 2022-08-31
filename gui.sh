6795c86a92a5c033dca539bab1f732cb8197d412
jnnanni <jnnanni@umich.edu>
2022-08-30 20:33:48 -0400

    Made gui.sh match the main repo

--- gui.sh
+++ gui.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Ensure proper working directory
+roscd mrover/src/teleop/gui
+#Ensure proper NODE_PATH
+export NODE_PATH=/usr/lib/nodejs:/usr/share/nodejs
+
+#Install web packages if no node_modules folder
 if [ ! -d "./node_modules/" ] 
 then
     yarnpkg install
